e5587f31ba4b8f088bdff2315a261683148b0fb7
Quentin Gérome <q.gerome@whatever-company.com>
2019-11-15 15:38:02 +0100

    fix: accept row to only have a text

--- examples/value.jsx
+++ examples/value.jsx
@@ -7,38 +7,34 @@ export default (
 		<document>
 			<table>
 				<tr>
-					<td colspan={2}>
+					<td>
 						<paragraph>
 							<cursor />
 							<text>Cell 1,1</text>
 						</paragraph>
 					</td>
-					<td>
+					<td colspan={2}>
+						<paragraph>Cell 1,2</paragraph>
 						<paragraph>Cell 1,3</paragraph>
 					</td>
 				</tr>
 				<tr>
-					<td>
+					<td rowspan={2}>
 						<paragraph>Cell 2,1</paragraph>
+						<paragraph>Cell 2,2</paragraph>
 					</td>
 					<td>
 						<paragraph>Cell 2,2</paragraph>
 					</td>
-					<td>
+					<td rowspan={2}>
 						<paragraph>Cell 2,3</paragraph>
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<paragraph>Cell 3,1</paragraph>
-					</td>
-					<td>
 						<paragraph>
 							<text>Cell 3,2</text>
 						</paragraph>
-					</td>
-					<td>
-						<paragraph>Cell 3,3</paragraph>
 					</td>
 				</tr>
 			</table>
