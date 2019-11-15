edb40b41417eb1848e9f9c1d4e4746d962d83bf7
Quentin Gérome <q.gerome@whatever-company.com>
2019-11-15 15:42:46 +0100

    Revert "fix: accept row to only have a text"
    This reverts commit e5587f31ba4b8f088bdff2315a261683148b0fb7.

--- examples/value.jsx
+++ examples/value.jsx
@@ -7,34 +7,38 @@ export default (
 		<document>
 			<table>
 				<tr>
-					<td>
+					<td colspan={2}>
 						<paragraph>
 							<cursor />
 							<text>Cell 1,1</text>
 						</paragraph>
 					</td>
-					<td colspan={2}>
-						<paragraph>Cell 1,2</paragraph>
+					<td>
 						<paragraph>Cell 1,3</paragraph>
 					</td>
 				</tr>
 				<tr>
-					<td rowspan={2}>
+					<td>
 						<paragraph>Cell 2,1</paragraph>
-						<paragraph>Cell 2,2</paragraph>
 					</td>
 					<td>
 						<paragraph>Cell 2,2</paragraph>
 					</td>
-					<td rowspan={2}>
+					<td>
 						<paragraph>Cell 2,3</paragraph>
 					</td>
 				</tr>
 				<tr>
 					<td>
+						<paragraph>Cell 3,1</paragraph>
+					</td>
+					<td>
 						<paragraph>
 							<text>Cell 3,2</text>
 						</paragraph>
+					</td>
+					<td>
+						<paragraph>Cell 3,3</paragraph>
 					</td>
 				</tr>
 			</table>
