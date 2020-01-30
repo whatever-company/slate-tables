1aa3ca0a2ecc5aedbf1c748268886ffd4bce7535
Tanguy Charlier <t.charlier@whatever-company.com>
2020-01-30 15:13:41 +0100

    chore(Tables): normalize overflowing rowspans

---
+++ tests/fixtures/validation/rowspan-in-last-row.jsx
@@ -0,0 +1,47 @@
+/** @jsx h */
+
+import { h } from 'testutils/hyperscript'
+
+export default () => {}
+
+export const value = (
+	<value>
+		<document>
+			<table>
+				<tr>
+					<td rowspan={2}>
+						<paragraph>
+							<text>lorem ipsum</text>
+						</paragraph>
+					</td>
+					<td>
+						<paragraph>
+							<text />
+						</paragraph>
+					</td>
+				</tr>
+			</table>
+		</document>
+	</value>
+)
+
+export const output = (
+	<value>
+		<document>
+			<table>
+				<tr>
+					<td rowspan={1}>
+						<paragraph>
+							<text>lorem ipsum</text>
+						</paragraph>
+					</td>
+					<td>
+						<paragraph>
+							<text />
+						</paragraph>
+					</td>
+				</tr>
+			</table>
+		</document>
+	</value>
+)
